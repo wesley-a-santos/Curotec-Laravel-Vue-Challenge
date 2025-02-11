a1f43ee82436131c2268587fd58e67ca85fe6e9e
Wesley Alves da Silva Santos <wesley.a.santos@gmail.com>
2025-02-11 13:38:19 -0300

    feat: Added isAdmin attribute for sending to front-end

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -29,10 +29,16 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = $request->user();
+        $user['isAdmin'] =  $user->isAdmin();
+
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user(),
+                'user' => $user,
+            ],
+            'flash' => [
+                'message' => fn () => $request->session()->get('message')
             ],
         ];
     }
