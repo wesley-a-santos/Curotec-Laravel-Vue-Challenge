a31af3122ba58af561e0df2af5f946da64c9fa20
Wesley Alves da Silva Santos <wesley.a.santos@gmail.com>
2025-02-11 16:57:41 -0300

    fix(HandleInertiaRequests): simplify user props sharing, to fix logout error

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -25,19 +25,28 @@ class HandleInertiaRequests extends Middleware
     /**
      * Define the props that are shared by default.
      *
+     * This method is called on every request and can be used to share
+     * common data between components. The data can be accessed in components
+     * via the `$page.props` property.
+     *
      * @return array<string, mixed>
      */
     public function share(Request $request): array
     {
-        $user = $request->user();
-        $user['isAdmin'] =  $user->isAdmin();
-
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $user,
+                // The user object is shared by default so that we can access
+                // it in any component.
+                'user' => $request->user(),
+
+                // This boolean is used to conditionally render elements that
+                // should only be visible to admins.
+                'isAdmin' => $request->user()?->isAdmin(),
             ],
             'flash' => [
+                // The flash message is shared by default so that we can access
+                // it in any component. It is cleared after the first render.
                 'message' => fn () => $request->session()->get('message')
             ],
         ];
