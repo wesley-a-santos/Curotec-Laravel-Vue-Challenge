e121f3d04a853d733ca67c6238958962efc4ef0b
Wesley Alves da Silva Santos <wesley.a.santos@gmail.com>
2025-02-11 12:48:12 -0300

    feat: Created front-end components for links and buttons

---
+++ resources/js/Components/SelectMenu.vue
@@ -0,0 +1,19 @@
+<script setup>
+const model = defineModel({
+    required: true,
+    selected: null,
+});
+
+const props = defineProps({
+    itens: {
+        type: Object,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <select v-model="model" class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
+        <option v-for="item in itens" :value="item.id" :key="item.id">{{ item.name }}</option>
+    </select>
+</template>
