0d9aee556c38265dcd40a9c92507231305235e8d
Wesley Alves da Silva Santos <wesley.a.santos@gmail.com>
2025-02-11 18:05:24 -0300

    fix: Removed roles, genders and users api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\V1\GenderController;
-use App\Http\Controllers\Api\V1\RoleController;
-use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,13 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-
-Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
-    Route::get('/roles', [RoleController::class, 'index']);
-
-    Route::get('/genders', [GenderController::class, 'index']);
-
-    Route::apiResource('/users', UserController::class);
 });
