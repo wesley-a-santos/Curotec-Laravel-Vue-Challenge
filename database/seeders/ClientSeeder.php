02ecb61fbc468a7027cbdf61719d46efb2078e8d
Wesley Alves da Silva Santos <wesley.a.santos@gmail.com>
2025-02-12 15:14:58 -0300

    test: add clients generation in the User seeder

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Seeder;
 class ClientSeeder extends Seeder
 {
     use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      *
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\Client;
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        User::factory()->count(10)->create();
+        User::factory()->count(10)
+            ->has(Client::factory()->count(15))
+            ->create();
     }
 }
